17842f3db71ce797f3ee39c40130cb8fe2391c16
comp21823 <93668889+anandavenkatesh-a@users.noreply.github.com>
2025-10-18 11:31:53 +0530

    Refactor sparseTable.cpp

--- sparseTable.cpp
+++ sparseTable.cpp
@@ -1,25 +1,17 @@
-#define N 
-#define M 
-
-int arr[N];
-
-int msb(int n){
-    int k = __builtin_clzll(n);
-    return 63 - k;	
-}
-
-int sparse[N][M];
-
-int n;
+const int N = 200000;
+const int B = floor(log2(N)) + 1;
+int sparse[1+N][1 + B];
+int arr[1+N];
+int n,q; 
 
 void build_sparse(){
     //preprocessing
-	for (int j = 0; j < M; ++j) {
-		for (int i = 0; i < n; ++i) {
+	for (int j = 0; j <= B; ++j) {
+		for (int i = 1; i <= n; ++i) {
 			if (j == 0) {
 				sparse[i][j] = arr[i];
 			}
-			else if ((i + (1 << j)) <= n) {
+			else if ((i + (1 << j)) <= n+1) {
 			    sparse[i][j] = min(sparse[i][j - 1], sparse[i + (1 << (j - 1))][j - 1]);
 			}
 		}
