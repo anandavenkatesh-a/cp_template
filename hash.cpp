9fbf71c7043fdbb6b99513fab13177426178f182
comp21823 <93668889+anandavenkatesh-a@users.noreply.github.com>
2025-10-23 15:39:22 +0530

    Refactor hash.cpp to use Class.

--- hash.cpp
+++ hash.cpp
@@ -1,48 +1,66 @@
-const int base = 29;
-const int mod1 = 1e9+7;
-const int mod2 = 1e9+9;
- 
-string text,pattern;
- 
-#define N 1000000
-pii prefix[1+N];
-int pw1[1+N];
-int pw2[1+N];
-void _build(){
-	//text
-    pw1[0] = 1;
-    pw2[0] = 1;
+class StringHash {
+private:
+    string text;
+    int base;
+    int mod1, mod2;
+    vector<pair<int, int>> prefix;
+    vector<int> pw1, pw2;
     
-    for(int i = 1;i <= text.size();++i){
-    	pw1[i] = mul(pw1[i-1],base,mod1);
-    	pw2[i] = mul(pw2[i-1],base,mod2);
+    void build() {
+        int n = text.size();
+        
+        // Initialize power arrays
+        pw1.resize(n + 1);
+        pw2.resize(n + 1);
+        pw1[0] = 1;
+        pw2[0] = 1;
+        
+        for (int i = 1; i <= n; ++i) {
+            pw1[i] = mul(pw1[i-1], base, mod1);
+            pw2[i] = mul(pw2[i-1], base, mod2);
+        }
+        
+        // Build prefix hash
+        prefix.resize(n + 1);
+        prefix[0] = {0, 0};
+        
+        for (int i = 1; i <= n; ++i) {
+            prefix[i].first = prefix[i-1].first;
+            add_self(prefix[i].first, mul(text[i-1] - 'a', pw1[i-1], mod1), mod1);
+            
+            prefix[i].second = prefix[i-1].second;
+            add_self(prefix[i].second, mul(text[i-1] - 'a', pw2[i-1], mod2), mod2);
+        }
+    }
+
+public:
+    // Constructor
+    StringHash(const string& s, int b = 29, int m1 = 1e9+7, int m2 = 1e9+9) 
+        : text(s), base(b), mod1(m1), mod2(m2) {
+        build();
     }
     
-	prefix[0].ff = 0;
-	prefix[0].ss = 0;
-	
-	for(int i = 1;i <= text.size();++i){
-		prefix[i].ff = prefix[i-1].ff;
-		add_self(prefix[i].ff,mul(text[i-1]-'a',pw1[i-1],mod1),mod1);
-		
-		prefix[i].ss = prefix[i-1].ss;
-		add_self(prefix[i].ss,mul(text[i-1]-'a',pw2[i-1],mod2),mod2);
-	}
-}
- 
-pii _hash(){
-	//patern
-	int ans1 = 0,ans2 = 0;
-	for(int i = 0;i < pattern.size();++i){
-		add_self(ans1,mul(pattern[i]-'a',pw1[i],mod1),mod1);
-		add_self(ans2,mul(pattern[i]-'a',pw2[i],mod2),mod2);
-	}
-	return {ans1,ans2};
-}
- 
-pii _hash(int l,int r){
-	//text
-	int ans1 = mul(sub(prefix[r].ff,prefix[l-1].ff,mod1),modInverse(pw1[l-1],mod1),mod1);
-	int ans2 = mul(sub(prefix[r].ss,prefix[l-1].ss,mod2),modInverse(pw2[l-1],mod2),mod2);
-	return {ans1,ans2};
-}
+    // Get hash of substring [l, r] (1-indexed)
+    pair<int, int> getHash(int l, int r) {
+        int ans1 = mul(sub(prefix[r].first, prefix[l-1].first, mod1), 
+                      modInverse(pw1[l-1], mod1), mod1);
+        int ans2 = mul(sub(prefix[r].second, prefix[l-1].second, mod2), 
+                      modInverse(pw2[l-1], mod2), mod2);
+        return {ans1, ans2};
+    }
+    
+    // Get hash of entire string
+    pair<int, int> getHash() {
+        return getHash(1, text.size());
+    }
+    
+    // Get text
+    string getText() const {
+        return text;
+    }
+    
+    // Get length
+    int length() const {
+        return text.size();
+    }
+};
